2d74b4a036695bb147b21bfadbfe07fd0e4ef45e
Alex1989-star <bio181@yandex.ru>
2021-05-21 17:00:00 +0300

    Added UI components

--- ChangingTheViewColor/ViewController.swift
+++ ChangingTheViewColor/ViewController.swift
@@ -9,13 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var redSlider: UISlider!
     
-
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var meinView: UIView!
+    
+    @IBOutlet weak var numberLabelRed: UILabel!
+    
+    @IBOutlet weak var numberLabelGreen: UILabel!
+    
+    @IBOutlet weak var numberLabelBlue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        meinView.layer.cornerRadius = 10
+        
+        
+        redSlider.maximumValue = 1
+        redSlider.minimumValue = 0
+        numberLabelRed.font = numberLabelRed.font.withSize(20)
+        
+        greenSlider.maximumValue = 1
+        greenSlider.minimumValue = 0
+        numberLabelGreen.font = numberLabelGreen.font.withSize(20)
+        
+        blueSlider.maximumValue = 1
+        blueSlider.minimumValue = 0
+        numberLabelBlue.font = numberLabelBlue.font.withSize(20)
+        
+        
+        
     }
+    
+    func changeColors() {
+        meinView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
 
-
+    @IBAction func redSliderChangedColor() {
+        numberLabelRed.text = String(format: "%.2f", redSlider.value)
+        
+        changeColors()
+        
+        
+    }
+    
+    @IBAction func greenSliderChangedColor() {
+        numberLabelGreen.text = String(format: "%.2f", greenSlider.value)
+        changeColors()
+    }
+    
+    
+    @IBAction func blieSliderChangedColor() {
+        numberLabelBlue.text = String(format: "%.2f", blueSlider.value)
+        changeColors()
+        
+        
+    }
+    
 }
 
