724b8aeaa175befef0f9f4bc23834312102182c7
Alex1989-star <bio181@yandex.ru>
2021-05-23 11:47:45 +0300

    Added UI changes

--- ChangingTheViewColor/ViewController.swift
+++ ChangingTheViewColor/ViewController.swift
@@ -33,16 +33,23 @@ class ViewController: UIViewController {
         numberLabelRed.font =
             numberLabelRed.font.withSize(17)
         
+        redSlider.thumbTintColor = .red
+        redSlider.maximumTrackTintColor = .opaqueSeparator
+        
         
         greenSlider.maximumValue = 1
         greenSlider.minimumValue = 0
         numberLabelGreen.font = numberLabelGreen.font.withSize(17)
         
+        greenSlider.thumbTintColor = .green
+        greenSlider.maximumTrackTintColor = .opaqueSeparator
+        
         blueSlider.maximumValue = 1
         blueSlider.minimumValue = 0
         numberLabelBlue.font = numberLabelBlue.font.withSize(17)
         
-        
+        blueSlider.thumbTintColor = .blue
+        blueSlider.maximumTrackTintColor = .opaqueSeparator
         
     }
     
@@ -50,13 +57,11 @@ class ViewController: UIViewController {
         meinView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
-
     @IBAction func redSliderChangedColor() {
         numberLabelRed.text = String(format: "%.2f", redSlider.value)
         
         changeColors()
-        
-        
+    
     }
     
     @IBAction func greenSliderChangedColor() {
